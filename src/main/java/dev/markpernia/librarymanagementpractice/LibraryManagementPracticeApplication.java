66225141797424e11ca05cbfe914f183f37927a3
markpernia <perniamanila@gmail.com>
2023-12-21 19:53:12 +0100

    Added sample Book records

--- src/main/java/dev/markpernia/librarymanagementpractice/LibraryManagementPracticeApplication.java
+++ src/main/java/dev/markpernia/librarymanagementpractice/LibraryManagementPracticeApplication.java
@@ -1,7 +1,9 @@
 package dev.markpernia.librarymanagementpractice;
 
 import dev.markpernia.librarymanagementpractice.entity.Author;
+import dev.markpernia.librarymanagementpractice.entity.Book;
 import dev.markpernia.librarymanagementpractice.repository.AuthorRepository;
+import dev.markpernia.librarymanagementpractice.repository.BookRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -13,10 +15,13 @@ import org.springframework.context.annotation.Bean;
 public class LibraryManagementPracticeApplication implements CommandLineRunner {
 
     private AuthorRepository authorRepository;
+    private BookRepository bookRepository;
 
     @Autowired
-    public LibraryManagementPracticeApplication(AuthorRepository authorRepository) {
+    public LibraryManagementPracticeApplication(AuthorRepository authorRepository,
+                                                BookRepository bookRepository) {
         this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
     }
 
     public static void main(String[] args) {
@@ -52,10 +57,54 @@ public class LibraryManagementPracticeApplication implements CommandLineRunner {
         marquez.setName("Gabriel Garcia Marquez");
         marquez.setEmail("gg.marquez@gmail.com");
 
+        Book hamlet = new Book();
+        hamlet.setTitle("Hamlet");
+        hamlet.setIsbn("978-0743477123");
+        hamlet.setAuthor(shakespeare);
+
+        Book sevenNovels = new Book();
+        sevenNovels.setTitle("Seven Novels");
+        sevenNovels.setIsbn("9781435167964");
+        sevenNovels.setAuthor(austen);
+
+        Book nineteenEightyFour = new Book();
+        nineteenEightyFour.setTitle("1984");
+        nineteenEightyFour.setIsbn("9780141036144");
+        nineteenEightyFour.setAuthor(orwell);
+
+        Book harryPotter = new Book();
+        harryPotter.setTitle("Harry Potter and the Philosopher's Stone");
+        harryPotter.setIsbn("978-1408855652");
+        harryPotter.setAuthor(rowling);
+
+        Book harryPotterChamber = new Book();
+        harryPotterChamber.setTitle("Harry Potter and the Chamber of Secrets");
+        harryPotterChamber.setIsbn("978-1408855669");
+        harryPotterChamber.setAuthor(rowling);
+
+        Book harryPotterPrisoner = new Book();
+        harryPotterPrisoner.setTitle("Harry Potter and the Prisoner of Azkaban");
+        harryPotterPrisoner.setIsbn("978-1408855676");
+        harryPotterPrisoner.setAuthor(rowling);
+
+        Book oneHundredYearsOfSolitude = new Book();
+        oneHundredYearsOfSolitude.setTitle("One Hundred Years of Solitude");
+        oneHundredYearsOfSolitude.setIsbn("9780241968581");
+        oneHundredYearsOfSolitude.setAuthor(marquez);
+
         authorRepository.save(shakespeare);
         authorRepository.save(austen);
         authorRepository.save(orwell);
         authorRepository.save(rowling);
         authorRepository.save(marquez);
+
+        bookRepository.save(hamlet);
+        bookRepository.save(sevenNovels);
+        bookRepository.save(nineteenEightyFour);
+        bookRepository.save(harryPotter);
+        bookRepository.save(oneHundredYearsOfSolitude);
+        bookRepository.save(harryPotterChamber);
+        bookRepository.save(harryPotterPrisoner);
+
     }
 }
