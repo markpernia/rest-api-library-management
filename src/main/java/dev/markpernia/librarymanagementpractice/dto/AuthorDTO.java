504fb4485cf413742670991f19884fa4df540123
markpernia <perniamanila@gmail.com>
2023-12-21 13:31:31 +0100

    added AuthorDTO and ErrorDTO

---
+++ src/main/java/dev/markpernia/librarymanagementpractice/dto/AuthorDTO.java
@@ -0,0 +1,12 @@
+package dev.markpernia.librarymanagementpractice.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AuthorDTO {
+
+    private String name;
+    private String email;
+}
